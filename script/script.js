baf7b89bfedcf545ebdfa28837a1c89fd629db68
Полина Илянкина <polinailankina@MacBook-Pro-Polina.local>
2023-07-15 23:53:18 +0500

    fixed cover

--- script/script.js
+++ script/script.js
@@ -1,15 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
     const paragraph = document.getElementById('paragraph');
+    const subtitle = document.querySelector('.cover__subtitle');
     let originalContent = paragraph.innerHTML;
+    let originalSubtitle = subtitle.innerHTML;
     function handleWindowResize() {
         const screenWidth = window.innerWidth;
-
+        console.log(screenWidth);
         if (screenWidth > 320) {
+            subtitle.innerHTML = subtitle.innerHTML.replace(/&nbsp;/g, ' ');
             paragraph.innerHTML = paragraph.innerHTML.replace(/&nbsp;и&nbsp;тандем/g, ' и&nbsp;тандем');
-        }  else {
+        } else {
+            subtitle.innerHTML = originalSubtitle;
             paragraph.innerHTML = originalContent;
         }
-       
+
     }
     window.addEventListener('resize', handleWindowResize);
 
